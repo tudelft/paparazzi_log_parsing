cec384b229c5525ed988f089d5116a6e6f18ab29
guidoAI <guido.de.croon@gmail.com>
2016-11-19 22:22:57 +0100

    Aligning data of different length

--- python/readlog.py
+++ python/readlog.py
@@ -19,8 +19,69 @@ class rotorcraft_fp_class:
         return 'hello world'
 
 
+def time_align_data(time1, v1, time2, v2):
+    """
+    Takes two vectors with measurements of different length (v1, v2) - recorded at different time steps (time1, time2)
+    Returns the vectors and times with length equal to the maximum length, linearly interpolating values in v
+    """
 
+    # 1) join the two time lists into one series
+    # 2) per time step see if it exists in vector v1 / v2
+    # 3) if it does not exist, insert a linearly interpolated element
 
+    # 1)
+    # first take out double times:
+    time1, inds1 = np.unique(time1, return_index=True);
+    v1 = v1[inds1];
+    time2, inds2 = np.unique(time2, return_index=True);
+    v2 = v2[inds2];
+    # then concatenate, get all unique times, and sort:
+    times = np.concatenate([time1, time2]);
+    times = np.unique(times);
+    sorted_times = np.sort(times, axis=None);
+    # go over all elements:
+    for t in sorted_times:
+        # 2) / 3)
+        [v1, time1] = check_for_t(v1, time1, t);
+        [v2, time2] = check_for_t(v2, time2, t);
+
+    return [time1, v1, time2, v2];
+
+def check_for_t(v, time, t):
+    pad_value = 0;
+    # 2)
+    inds = np.where(time == t);
+    if inds[0].size == 0:
+        # 3)
+        # time step does not exist:
+        inds_l = np.where(time > t);
+        inds_s = np.where(time < t);
+        if inds_l[0].size == 0:
+            # larger than the other times, append at the back:
+            v.append(pad_value);
+            time.append(t);      
+        elif inds_s[0].size == 0:
+            # smaller than the other times, insert at the front:
+            v = np.insert(v, 0, pad_value);
+            time = np.insert(time, 0, t);
+        else:
+            # value of the largest smaller time step
+            v_ts = v[inds_s[0][-1]];
+            # value of the smallest larger time step
+            v_tl = v[inds_l[0][0]];
+            # t smaller / larger
+            t_s = time[inds_s[0][-1]];
+            t_l = time[inds_l[0][0]];
+            # linear interpolation:
+            alpha = (t - t_s) / (t_l - t_s);
+            new_val = alpha * v_tl + (1-alpha) * v_ts;
+            # insert the values at the right place:
+            v = np.insert(v, inds_l[0][0], new_val);
+            time = np.insert(time, inds_l[0][0], t);
+
+    # return the possibly modified vector:
+    return [v, time];
+                
 def plot_log_file(filename, nr):
 
     gps_int = []
@@ -118,15 +179,27 @@ def plot_log_file(filename, nr):
         height = divergence_landing[:,8];
         N = 30; # smoothing window size:
         height = np.convolve(height, np.ones((N,))/N, mode='same');
-        n_steps = 5;
+        n_steps = 10;
         dt = time_steps[n_steps:-1] - time_steps[0:-n_steps-1];
         velocity = np.divide(height[n_steps:-1] - height[0:-n_steps-1], dt);
         div_truth = np.divide(velocity, height[n_steps:-1]);
+
+        time_gps = g[:,0];
+        height_gps = g[:,8];
+
+        print 'sizes = %d, %d\n' % (time_steps.size, time_gps.size);
+        [time_steps, height, time_gps, height_gps] = time_align_data(time_steps, height, time_gps, height_gps);
+        print 'sizes = %d, %d\n' % (time_steps.size, time_gps.size);
+
         f = plt.figure();
-        plt.plot(time_steps[n_steps:-1], velocity);
+        #plt.plot(time_steps[n_steps:-1], velocity);
         #plt.plot(time_steps, height);
-        #plt.plot(time_steps[1:-1], div_truth, time_steps[1:-1], div_vision[1:-1]);
+        # typically our interest:
+        # plt.plot(time_steps[n_steps:-1], div_truth, time_steps[n_steps:-1], div_vision[n_steps:-1]);
+        plt.plot(time_steps, height, time_steps, height_gps/1000.0);
+        plt.legend(['height sonar', 'height optitrack'])
         plt.show();
+        # For debugging:        
         pdb.set_trace();
         
 
