934378ed3693355890bc345b3f815956aa8b32a2
Freek van Tienen <freek.v.tienen@gmail.com>
2022-09-07 17:14:56 +0200

    Fix energy plot

--- new_matlab/plot_energy.m
+++ new_matlab/plot_energy.m
@@ -1,28 +1,23 @@
 function plot_energy(ac_data, vert)
     % Plot the Energy message
     if isfield(ac_data, 'ENERGY')
-        figure(10);
-        ax1 = subplot(3,1,1);
+        ax1 = subplot(2,1,1);
+        yyaxis left
         plot(ac_data.ENERGY.timestamp, [ac_data.ENERGY.voltage]);
+        ylabel('Voltage [V]');
+        yyaxis right
+        plot(ac_data.ENERGY.timestamp, [ac_data.ENERGY.current]);
+        ylabel('Current [A]');
+        
         if exist('vert','var')
             hold on;
             plot([vert'; vert'], repmat(ylim',1,size(vert,1)), '--r');
         end
         title('Energy Voltage');
         xlabel('Time [s]');
-        legend('Voltage [V]');
+        legend('Voltage [V]', 'Current [A]');
 
-        ax2 = subplot(3,1,2);
-        plot(ac_data.ENERGY.timestamp, [ac_data.ENERGY.current]);
-        if exist('vert','var')
-            hold on;
-            plot([vert'; vert'], repmat(ylim',1,size(vert,1)), '--r');
-        end
-        title('Energy Current');
-        xlabel('Time [s]');
-        legend('Current [A]');
-
-        ax3 = subplot(3,1,3);
+        ax2 = subplot(2,1,2);
         plot(ac_data.ENERGY.timestamp, [ac_data.ENERGY.power]);
         if exist('vert','var')
             hold on;
@@ -33,6 +28,6 @@ function plot_energy(ac_data, vert)
         legend('Power [W]');
 
         sgtitle('Energy')
-        linkaxes([ax1, ax2, ax3],'x')
+        linkaxes([ax1, ax2],'x')
     end
     end
