fcd5b960ee289f4c7bb3ef9188e468ce477cd0d5
NoahWe <noah.wechtler@hotmail.com>
2024-08-22 14:23:43 +0200

    add hysteresis to rotwing flight envelope

--- matlab/plotters/plot_rotwing_flight_envelope.m
+++ matlab/plotters/plot_rotwing_flight_envelope.m
@@ -1,11 +1,11 @@
-function plot_rotwing_flight_envelope(params)
+function plot_rotwing_flight_envelope(params, use_hysteresis)
 %PLOT_ROTWING_FLIGHT_ENVELOPE Summary of this function goes here
 %   Detailed explanation goes here
 
 skew = 0:0.25:90;
 
-min_airspeed = params.FW_QUAD_MIN_AIRSPEED * (skew - 55) / (90 - 55);
-max_airspeed = params.QUAD_MAX_AIRSPEED + (params.FW_MAX_AIRSPEED - params.QUAD_MAX_AIRSPEED) * skew / 85;
+min_airspeed = params.FW_QUAD_MIN_AIRSPEED * (skew - params.MIN_AIRSPEED_SLOPE_START_ANGLE) / (90 - params.MIN_AIRSPEED_SLOPE_START_ANGLE);
+max_airspeed = params.QUAD_MAX_AIRSPEED + (params.FW_MAX_AIRSPEED - params.QUAD_MAX_AIRSPEED) * skew / params.FW_SKEW_ANGLE;
 
 min_airspeed(min_airspeed < 0) = 0;
 min_airspeed(min_airspeed > params.FW_MIN_AIRSPEED) = params.FW_MIN_AIRSPEED;
@@ -20,17 +20,25 @@ constants = [params.FW_MIN_AIRSPEED, params.FW_QUAD_MIN_AIRSPEED, params.FW_CRUI
 labels = {"FW\_MIN\_AIRSPEED", "FW\_QUAD\_MIN\_AIRSPEED", "FW\_CRUISE\_AIRSPEED", "FW\_MAX\_AIRSPEED", ...
     "QUAD\_NOPUSH\_AIRSPEED", "QUAD\_MAX\_AIRSPEED", "SKEW\_START\_AIRSPEED"};
 
-skewing_x_vals = [0, 0, 55, 55, 90, 90];
+skewing_x_vals = [0, 0, params.SKEW_ANGLE_STEP, params.SKEW_ANGLE_STEP, 90, 90];
 skewing_y_vals = [0, params.SKEW_START_AIRSPEED, params.SKEW_START_AIRSPEED, params.QUAD_MAX_AIRSPEED, params.FW_MIN_AIRSPEED, params.FW_MAX_AIRSPEED];
+
+hysteresis_x_vals = [params.SKEW_ANGLE_STEP, params.SKEW_ANGLE_STEP, 0];
+hysteresis_y_vals = [params.SKEW_START_AIRSPEED, params.SKEW_START_AIRSPEED - params.HYSTERESIS_THRESHOLD, params.SKEW_START_AIRSPEED - params.HYSTERESIS_THRESHOLD];
 
 figure;
 plot(skew, min_airspeed, "Color", 'blue', LineWidth=2)
 hold on
 plot(skew, max_airspeed, "Color", 'red', LineWidth=2)
 plot(skewing_x_vals, skewing_y_vals, "Color", 'black', 'LineWidth', 2)
+if use_hysteresis
+    plot(hysteresis_x_vals, hysteresis_y_vals, "Color", 'black', 'LineWidth', 2, 'LineStyle', '--')
+end
 hold on;
 yline(constants, '--', labels)
-legend(["min airspeed" "max airspeed" "Skewing path"])
+legend(["min airspeed" "max airspeed" "Skewing path" "Hysteresis"])
+xlabel("Skew angle [deg]")
+ylabel("Airspeed [m/s]")
 
 
 end
--- matlab/rotwing_flight_envelope.m
+++ matlab/rotwing_flight_envelope.m
@@ -7,9 +7,13 @@ Rotwing_v3.FW_MIN_AIRSPEED = 17;
 Rotwing_v3.FW_QUAD_MIN_AIRSPEED = 15;
 Rotwing_v3.FW_CRUISE_AIRSPEED = 19;
 Rotwing_v3.FW_MAX_AIRSPEED = 22;
-Rotwing_v3.QUAD_NOPUSH_AIRSPEED = 8;
+Rotwing_v3.QUAD_NOPUSH_AIRSPEED = 5;
 Rotwing_v3.QUAD_MAX_AIRSPEED = 15;
 Rotwing_v3.SKEW_START_AIRSPEED = 10;
+Rotwing_v3.SKEW_ANGLE_STEP = 55.0;
+Rotwing_v3.MIN_AIRSPEED_SLOPE_START_ANGLE = 30;
+Rotwing_v3.FW_SKEW_ANGLE = 85;
+Rotwing_v3.HYSTERESIS_THRESHOLD = 2;
 
 % Rotwing_25kg.FW_MIN_AIRSPEED = ;
 % Rotwing_25kg.FW_QUAD_MIN_AIRSPEED = ;
@@ -18,6 +22,12 @@ Rotwing_v3.SKEW_START_AIRSPEED = 10;
 % Rotwing_25kg.QUAD_NOPUSH_AIRSPEED = ;
 % Rotwing_25kg.QUAD_MAX_AIRSPEED = ;
 % Rotwing_25kg.SKEW_START_AIRSPEED = ;
+% Rotwing_25kg.SKEW_ANGLE_STEP = 55;
+% Rotwing_25kg.MIN_AIRSPEED_SLOPE_START_ANGLE = 30;
+% Rotwing_25kg.FW_SKEW_ANGLE = 85;
+% Rotwing_25kg.HYSTERESIS_THRESHOLD = ; 
 
-plot_rotwing_flight_envelope(Rotwing_v3);
+use_hysteresis = true;
+
+plot_rotwing_flight_envelope(Rotwing_v3, use_hysteresis);
 % plot_rotwing_flight_envelope(Rotwing_25kg);
