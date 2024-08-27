9c654dda24e16770d707fa2f7913203529f454f5
NoahWe <noah.wechtler@hotmail.com>
2024-08-27 09:02:02 +0200

    25kg skew values

--- matlab/plotters/plot_rotwing_flight_envelope.m
+++ matlab/plotters/plot_rotwing_flight_envelope.m
@@ -1,4 +1,4 @@
-function plot_rotwing_flight_envelope(params, use_hysteresis)
+function plot_rotwing_flight_envelope(params, plot_title)
 %PLOT_ROTWING_FLIGHT_ENVELOPE Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -15,30 +15,29 @@ max_airspeed(max_airspeed > params.FW_MAX_AIRSPEED) = params.FW_MAX_AIRSPEED;
 
 constants = [params.FW_MIN_AIRSPEED, params.FW_QUAD_MIN_AIRSPEED, params.FW_CRUISE_AIRSPEED, ...
     params.FW_MAX_AIRSPEED, params.QUAD_NOPUSH_AIRSPEED, params.QUAD_MAX_AIRSPEED, ...
-    params.SKEW_START_AIRSPEED];
+    params.SKEW_UP_AIRSPEED, params.SKEW_DOWN_AIRSPEED];
 
 labels = {"FW\_MIN\_AIRSPEED", "FW\_QUAD\_MIN\_AIRSPEED", "FW\_CRUISE\_AIRSPEED", "FW\_MAX\_AIRSPEED", ...
-    "QUAD\_NOPUSH\_AIRSPEED", "QUAD\_MAX\_AIRSPEED", "SKEW\_START\_AIRSPEED"};
+    "QUAD\_NOPUSH\_AIRSPEED", "QUAD\_MAX\_AIRSPEED", "SKEW\_UP\_AIRSPEED", "SKEW\_DOWN\_AIRSPEED"};
 
 skewing_x_vals = [0, 0, params.SKEW_ANGLE_STEP, params.SKEW_ANGLE_STEP, 90, 90];
-skewing_y_vals = [0, params.SKEW_START_AIRSPEED, params.SKEW_START_AIRSPEED, params.QUAD_MAX_AIRSPEED, params.FW_MIN_AIRSPEED, params.FW_MAX_AIRSPEED];
+skewing_y_vals = [0, params.SKEW_UP_AIRSPEED, params.SKEW_UP_AIRSPEED, params.QUAD_MAX_AIRSPEED, params.FW_MIN_AIRSPEED, params.FW_MAX_AIRSPEED];
 
-hysteresis_x_vals = [params.SKEW_ANGLE_STEP, params.SKEW_ANGLE_STEP, 0];
-hysteresis_y_vals = [params.SKEW_START_AIRSPEED, params.SKEW_START_AIRSPEED - params.HYSTERESIS_THRESHOLD, params.SKEW_START_AIRSPEED - params.HYSTERESIS_THRESHOLD];
+hysteresis_x_vals = [params.SKEW_ANGLE_STEP, params.SKEW_ANGLE_STEP, 0, 0];
+hysteresis_y_vals = [params.SKEW_UP_AIRSPEED, params.SKEW_DOWN_AIRSPEED, params.SKEW_DOWN_AIRSPEED, 0];
 
 figure;
 plot(skew, min_airspeed, "Color", 'blue', LineWidth=2)
 hold on
 plot(skew, max_airspeed, "Color", 'red', LineWidth=2)
 plot(skewing_x_vals, skewing_y_vals, "Color", 'black', 'LineWidth', 2)
-if use_hysteresis
-    plot(hysteresis_x_vals, hysteresis_y_vals, "Color", 'black', 'LineWidth', 2, 'LineStyle', '--')
-end
+plot(hysteresis_x_vals, hysteresis_y_vals, "Color", 'black', 'LineWidth', 2, 'LineStyle', '--')
 hold on;
 yline(constants, '--', labels)
 legend(["min airspeed" "max airspeed" "Skewing path" "Hysteresis"])
 xlabel("Skew angle [deg]")
 ylabel("Airspeed [m/s]")
+title(plot_title)
 
 
 end
