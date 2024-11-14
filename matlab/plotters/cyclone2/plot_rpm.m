ec57a5555b7ec0fd79c7dd182b4e4d2059a855a5
ntouev <vaggelisntouros@gmail.com>
2024-11-14 16:00:18 +0100

    moved cyclone plotters to different directory

---
+++ matlab/plotters/cyclone2/plot_rpm.m
@@ -0,0 +1,38 @@
+function plot_rpm(ac_data, order)
+
+    rpm1 = ac_data.SERIAL_ACT_T4_IN.motor_1_rpm;
+    rpm2 = ac_data.SERIAL_ACT_T4_IN.motor_2_rpm;
+
+    tiledlayout(2, 1, 'Padding', 'compact', 'TileSpacing', 'compact');
+
+    % First subplot for RPM
+    ax1 = nexttile;
+    hold on; zoom on;
+    h1 = plot(ac_data.SERIAL_ACT_T4_IN.timestamp, rpm1, 'LineWidth', 1.5);
+    h2 = plot(ac_data.SERIAL_ACT_T4_IN.timestamp, rpm2, 'LineWidth', 1.5);
+    xlabel('Time (s)');
+    ylabel('RPM');
+    title('RPM');
+    grid on;
+
+    % Second subplot for DShot commands
+    ax2 = nexttile;
+    hold on; zoom on;
+    h3 = plot(ac_data.SERIAL_ACT_T4_OUT.timestamp, ac_data.SERIAL_ACT_T4_OUT.motor_1_dshot_cmd, 'LineWidth', 1.5);
+    h4 = plot(ac_data.SERIAL_ACT_T4_OUT.timestamp, ac_data.SERIAL_ACT_T4_OUT.motor_2_dshot_cmd, 'LineWidth', 1.5);
+    h5 = plot(ac_data.ROTORCRAFT_RADIO_CONTROL.timestamp, ac_data.ROTORCRAFT_RADIO_CONTROL.throttle, 'LineWidth', 1.5);
+    xlabel('Time (s)');
+    ylabel('DShot Command');
+    title('DShot Command');
+    grid on;
+
+    linkaxes([ax1,ax2],'x')
+
+    % background color fill for various flight modes
+    mode_values = ac_data.ROTORCRAFT_RADIO_CONTROL.mode;
+    mode_timestamps = ac_data.ROTORCRAFT_RADIO_CONTROL.timestamp;
+    fill_mode_regions(mode_values, mode_timestamps, {ax1, ax2});
+    legend([h1, h2, h3, h4, h5], {'RPM 1', 'RPM 2', 'DShot Cmd 1', 'DShot Cmd 2', 'Throttle'});
+
+    % hold off; % Needed?
+end
---
+++ matlab/plotters/cyclone2/plot_stab_attitude.m
@@ -0,0 +1,47 @@
+function plot_stab_attitude(ac_data, order)
+    if ~isfield(ac_data, 'STAB_ATTITUDE')
+        return
+    end
+
+    att = double(string(ac_data.STAB_ATTITUDE.att));
+    attref = double(string(ac_data.STAB_ATTITUDE.att_ref));
+
+    w = double(string(ac_data.STAB_ATTITUDE.angular_rate));
+    wref = double(string(ac_data.STAB_ATTITUDE.angular_rate_ref));
+
+    wdot = double(string(ac_data.STAB_ATTITUDE.angular_accel));
+    wdotref = double(string(ac_data.STAB_ATTITUDE.angular_accel_ref));
+
+    ax1 = subplot(3,1,1);
+    hold on; zoom on;
+    plot(ac_data.STAB_ATTITUDE.timestamp, wdot(:,1), 'color', '#0072BD');
+    plot(ac_data.STAB_ATTITUDE.timestamp, wdotref(:,1), '--', 'color', '#0072BD');
+    plot(ac_data.STAB_ATTITUDE.timestamp, wdot(:,2), 'color', '#D95319');
+    plot(ac_data.STAB_ATTITUDE.timestamp, wdotref(:,2), '--', 'color', '#D95319');
+    plot(ac_data.STAB_ATTITUDE.timestamp, wdot(:,3), 'color', '#EDB120');
+    plot(ac_data.STAB_ATTITUDE.timestamp, wdotref(:,3), '--', 'color', '#EDB120');
+    title('angular acceleration'); xlabel('time(s)'); ylabel('(rad/s^2)'); legend('pdot','pdotref','qdot','qdotref','rdot','rdotref')
+
+    ax2 = subplot(3,1,2);
+    hold on; zoom on;
+    plot(ac_data.STAB_ATTITUDE.timestamp, w(:,1), 'color', '#0072BD');
+    plot(ac_data.STAB_ATTITUDE.timestamp, wref(:,1), '--', 'color', '#0072BD');
+    plot(ac_data.STAB_ATTITUDE.timestamp, w(:,2), 'color', '#D95319');
+    plot(ac_data.STAB_ATTITUDE.timestamp, wref(:,2), '--', 'color', '#D95319');
+    plot(ac_data.STAB_ATTITUDE.timestamp, w(:,3), 'color', '#EDB120');
+    plot(ac_data.STAB_ATTITUDE.timestamp, wref(:,3), '--', 'color', '#EDB120');
+    title('angular rates'); xlabel('time(s)'); ylabel('(rad/s)'); legend('p','pref','q','qref','r','rref')
+
+    ax3 = subplot(3,1,3);
+    hold on; zoom on;
+    plot(ac_data.STAB_ATTITUDE.timestamp, att(:,1), 'color', '#0072BD');
+    plot(ac_data.STAB_ATTITUDE.timestamp, attref(:,1), '--', 'color', '#0072BD');
+    plot(ac_data.STAB_ATTITUDE.timestamp, att(:,2), 'color', '#D95319');
+    plot(ac_data.STAB_ATTITUDE.timestamp, attref(:,2), '--', 'color', '#D95319');
+    plot(ac_data.STAB_ATTITUDE.timestamp, att(:,3), 'color', '#EDB120');
+    plot(ac_data.STAB_ATTITUDE.timestamp, attref(:,3), '--', 'color', '#EDB120');
+    title('Euler angles'); xlabel('time(s)'); ylabel('(rad)'); legend('phi','phiref','theta','thetaref','psi','psiref')
+
+    sgtitle(['Stab attitude'])
+    linkaxes([ax1,ax2,ax3],'x')
+end
