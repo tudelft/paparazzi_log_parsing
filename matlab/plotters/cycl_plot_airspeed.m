0fbb1a8cf2a7423f0ca953f40249333ba08c0906
ntouev <vaggelisntouros@gmail.com>
2024-12-13 19:23:54 +0100

    added 6 m/s line

--- matlab/plotters/cycl_plot_airspeed.m
+++ matlab/plotters/cycl_plot_airspeed.m
@@ -4,6 +4,7 @@ function cycl_plot_airspeed(ac_data, order)
 
     hold on; grid on; zoom on;
     h1 = plot(ac_data.AIR_DATA.timestamp, ac_data.AIR_DATA.airspeed);
+    h2 = yline(6, 'g--', 'LineWidth', 1.5);
     xlabel('Time [s]');
     ylabel('Airspeed [m/s]');
     title('Airspeed');
@@ -12,6 +13,6 @@ function cycl_plot_airspeed(ac_data, order)
     mode_values = ac_data.ROTORCRAFT_RADIO_CONTROL.mode;
     mode_timestamps = ac_data.ROTORCRAFT_RADIO_CONTROL.timestamp;
     cycl_fill_mode_regions(mode_values, mode_timestamps, {ax});
-    legend(ax, h1, 'Airspeed');
+    legend(ax, [h1, h2], {'Airspeed [m/s]', '6 m/s'});
     
 end
