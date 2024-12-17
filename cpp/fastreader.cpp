13768a1f3ec96d0381aecb53dd13c51065273cd5
Christophe De Wagter <dewagter@gmail.com>
2024-12-17 13:29:53 +0100

    New trick of Freek

--- cpp/fastreader.cpp
+++ cpp/fastreader.cpp
@@ -21,10 +21,14 @@
 
 #include <boost/iostreams/device/mapped_file.hpp> // for mmap
 #include <boost/filesystem.hpp> // for is_empty
+#include <boost/regex.hpp>
+#include <boost/spirit/home/x3.hpp>
+#include <boost/spirit/include/qi_lazy.hpp>
 #include <algorithm>  // for std::find
 #include <iostream>   // for std::cout
-#include <tinyxml2.h> // for XMLDocument
-#include <iostream> // for std::cout
+#include <cstring>
+#include <tinyxml2.h>
+#include <iostream>
 #include <regex>
 
 #include <pprzlink/MessageDictionary.h>
@@ -32,295 +36,210 @@
 #include <pprzlink/Message.h>
 #include <pprzlink/MessageFieldTypes.h>
 
-#include <boost/spirit/home/x3.hpp>
-
-
 using namespace boost::spirit::x3;
 
-
-
-void parse_airframe_list(tinyxml2::XMLElement *root) {
-    int aircrafts = 0;
-    auto aircraft = root->FirstChildElement("conf")->FirstChildElement("aircraft");
-    while (aircraft != nullptr)
-    {
-        auto className = aircraft->Attribute("name", nullptr);
-        if (className == nullptr)
-        {
-            className = aircraft->Attribute("NAME", nullptr);
-        }
-        int classId = aircraft->IntAttribute("ac_id", -1);
-        if (classId == -1)
-        {
-            classId = aircraft->IntAttribute("AC_ID", -1);
-        }
-        if (className == nullptr || classId == -1)
-        {
-            std::cout << "aircraft has no name or ac_id.";
-        }
-        // std::cout << " - aircraft: " << className << " id: " << classId << "\n";
-        aircraft = aircraft->NextSiblingElement("aircraft");
-        aircrafts++;
-    }
-    std::cout << " - aircrafts: " << aircrafts << "\n";
-}
-
-
-void parse_message(std::string fieldsStr, pprzlink::MessageDictionary *dict)
+std::map<uint8_t, tinyxml2::XMLElement *> aircrafts;
+void parse_airframe_list(tinyxml2::XMLElement *root)
 {
-    // std::cout << " - message: " << fieldsStr << "\n";
-    if (fieldsStr.empty())
-    {
-        return;
-    }
-
-    std::regex fieldRegex("([^ ]+|\"[^\"]+\")");
-
-    std::smatch results;
-    std::vector<std::string> fields;
-    while (std::regex_search(fieldsStr, results, fieldRegex))
-    {
-        fields.push_back(results.str());
-        // std::cout << " - field: " << results.str() << "\n";
-        fieldsStr = results.suffix();
-    }
-
-    if (fields.size() < 3)
-    {
-        std::cout << " - message with wrong number of fields (expected 3 / got " << fields.size() << ")";
-        return;
-    }
-
-    pprzlink::MessageDefinition def = dict->getDefinition(fields[2]);
-    pprzlink::Message msg(def);
-
-    int argc = fields.size();
-    if (def.getNbFields() != (size_t)(argc - 3) )
-    {
-      std::stringstream sstr;
-      sstr << fields[2] << " message with wrong number of fields (expected " << def.getNbFields() << " / got " << argc - 3
-           << ")";
-      std::cout << (sstr.str());
-    }
-
-    for (int i = 3; i < argc; ++i)
-    {
-      const auto& field = def.getField(i - 3);
-      // Need deserializing string to build FieldValue
-
-      // For char arrays and strings remove possible quotes
-      if ((field.getType().getBaseType()==pprzlink::BaseType::STRING || (field.getType().getBaseType()==pprzlink::BaseType::CHAR && field.getType().isArray())) && fields[i][0]=='"')
-      {
-        std::string str(fields[i]);
-        //std::cout << str.substr(1,str.size()-2) << std::endl;
-        msg.addField(field.getName(),str.substr(1,str.size()-2));
-      }
-      else
-      {
-        std::stringstream sstr(fields[i]);
-        if (field.getType().isArray())
-        {
-          switch (field.getType().getBaseType())
-          {
-            case pprzlink::BaseType::NOT_A_TYPE:
-              throw std::logic_error("NOT_A_TYPE for field ");// + field.getName()); // + " in message " + fields[2]);
-              break;
-            case pprzlink::BaseType::CHAR:
-              msg.addField(field.getName(), fields[i]);
-            //   std::cout << "Wrong field format for a char[] "+std::string(fields[i]) + " " + field.getName() + " in message " + fields[2] << std::endl;
-              break;
-            case pprzlink::BaseType::INT8:
-            case pprzlink::BaseType::INT16:
-            case pprzlink::BaseType::INT32:
-            case pprzlink::BaseType::UINT8:
-            case pprzlink::BaseType::UINT16:
-            case pprzlink::BaseType::UINT32:
-            case pprzlink::BaseType::FLOAT:
-            case pprzlink::BaseType::DOUBLE:
-            {
-              // Parse all numbers as a double
-              std::vector<double> values;
-              while (!sstr.eof())
-              {
-                double val;
-                char c;
-                sstr >> val;
-                if (!sstr.eof()) {
-                    sstr >> c;
-                    if (c!=',')
-                    {
-                    std::cout << "Wrong format for array "+std::string(fields[i])+ " in message " + fields[2] + " with data: " + sstr.str() << std::endl;
-                    }
-                }
-                values.push_back(val);
-              }
-              msg.addField(field.getName(), values); // The value will be statically cast to the right type
-            }
-              break;
-            case pprzlink::BaseType::STRING:
-              msg.addField(field.getName(), fields[i]);
-              break;
-          }
-        }
-        else
-        {
-          switch (field.getType().getBaseType())
-          {
-            case pprzlink::BaseType::NOT_A_TYPE:
-              throw std::logic_error("NOT_A_TYPE for field " + field.getName() + " in message " + fields[2]);
-              break;
-            case pprzlink::BaseType::CHAR:
-            {
-              char val;
-              sstr >> val;
-              msg.addField(field.getName(), val);
-            }
-              break;
-            case pprzlink::BaseType::INT8:
-            case pprzlink::BaseType::INT16:
-            case pprzlink::BaseType::INT32:
-            case pprzlink::BaseType::UINT8:
-            case pprzlink::BaseType::UINT16:
-            case pprzlink::BaseType::UINT32:
-            case pprzlink::BaseType::FLOAT:
-            case pprzlink::BaseType::DOUBLE:
-            {
-              // Parse all numbers as a double
-              double val;
-              sstr >> val;
-              msg.addField(field.getName(), val); // The value will be statically cast to the right type
-            }
-              break;
-            case pprzlink::BaseType::STRING:
-              msg.addField(field.getName(), fields[i]);
-              break;
-          }
-        }
-      }
-    }
-
-
+  // char *ac_or_af = (root->FirstChildElement("conf")->FirstChildElement("aircraft"))? "aircraft" : "airframe";
+  // auto aircraft = root->FirstChildElement("conf")->FirstChildElement(ac_or_af);
+  // while (aircraft != nullptr) {
+  //   auto className = aircraft->Attribute("name", nullptr);
+  //   if (className == nullptr) {
+  //     className = aircraft->Attribute("NAME", nullptr);
+  //   }
+  //   int classId = aircraft->IntAttribute("ac_id", -1);
+  //   if (classId == -1) {
+  //     classId = aircraft->IntAttribute("AC_ID", -1);
+  //   }
+  //   if (className == nullptr || classId == -1) {
+  //     std::cout << "aircraft has no name or ac_id.";
+  //   }
+  //   std::cout << " - aircraft: " << className << " id: " << classId << "\n";
+  //   aircrafts[classId] = aircraft;
+  //   aircraft = aircraft->NextSiblingElement(ac_or_af);
+  // }
 }
 
+//using List =  boost::mpl::list<double, std::string>;
 
 
-
-
-
-int main(int argc, char* argv[])
+/* Generate a pprzlink message */
+pprzlink::Message get_msg(std::string name, pprzlink::MessageDictionary *dict,
+                          auto values)
 {
-    if (argc < 2)
-    {
-        std::cout << "Usage: " << argv[0] << " <filename>\n";
-        return 1;
-    }
+  pprzlink::MessageDefinition def = dict->getDefinition(name);
+  pprzlink::Message msg(def);
+  for (size_t i = 0; i < def.getNbFields(); i++) {
+    auto field = def.getField(i);
 
-
-    if (boost::filesystem::is_empty(argv[1])) {
-        std::cout << " - Empty DATA file\n";
-        return 0;
-    }
-
-    std::cout << "LOG: " << argv[1] << "\n";
-
-
-    // boost::iostreams::mapped_file mmap(argv[1], boost::iostreams::mapped_file::readonly);
-    // auto f = mmap.const_data();
-    // auto l = f + mmap.size();
-
-
-    // replace the .data in argv[1] with .log
-    std::string log_file = argv[1];
-    log_file.replace(log_file.end()-4, log_file.end(), "log");
-
-    std::cout << " - log_file = " << log_file << "\n";
-
-
-    if (boost::filesystem::is_empty(log_file)) {
-        std::cout << " - Empty LOG file\n";
-        return 0;
-    }
-
-
-    tinyxml2::XMLDocument xml;
-    xml.LoadFile(log_file.c_str());
-    // Get a link to the root element
-    tinyxml2::XMLElement *root = xml.RootElement();
-    std::string rootElem(root->Value());
-    if(rootElem!="configuration")
-    {
-        std::cout << "Root element is not configuration in xml messages file (found "+rootElem+").";
-        return 0;
-    }
-
-    // Load message definitions from *.LOG / messages.xml
-    pprzlink::MessageDictionary *dict = new pprzlink::MessageDictionary(root->FirstChildElement("protocol"));
-//    pprzlink::MessageDictionary *dict = new pprzlink::MessageDictionary("./pprzlink/message_definitions/v1.0/messages.xml");
-
-
-    // Load all aircraft names
-    parse_airframe_list(root);
-
-
-    const char* msg = "17.316 44 ROTORCRAFT_FP 0 0 107 0 0 51597 71 85 16 122 -87 102 16 0 0";
-    std::string message(msg);
-    parse_message(message, dict);
-
-
-
-    // const char *PIC = "[PFC] pic:";
-
-    // // Find the first occurrence of the string "[PFC] pic:"
-    // auto it = std::search(f, l, PIC, PIC + strlen(PIC));
-    // if (it != l) {
-    //     // Print the line where the string was found
-    //     auto start = it;
-    //     while (start != f && *start != '\n') start--;
-    //     start++;
-    //     auto end = it;
-    //     while (end != l && *end != '\n') end++;
-    //     std::cout << " - Found: " << std::string(start, end) << "\n";
-    //     return 0;
-    // }
-
-
-    // // Find the number of lines in the file
-    uintmax_t m_numLines = 0;   
-    // while (f && f!=l) {
-    //     if ((f = static_cast<const char*>(memchr(f, '\n', l-f)))) {
-
-    //         // Parse line
-    //         // const char* message = (static_cast<const char*>(f));
-    //         // parse_message(message, dict);
-
-    //         m_numLines++, f++;
-    //     }
-    // }
-
-
-    boost::filesystem::ifstream fileHandler(argv[1]);
-    std::string line;
-    while (getline(fileHandler, line)) {
-        parse_message(line, dict);
-        m_numLines++;
-        
-        // Print a single dot for every 1000 lines
-        if (m_numLines % 1000 == 0) {
-            std::cout << ".";
-            std::flush(std::cout);
+    switch (field.getType().getBaseType()) {
+      case pprzlink::BaseType::INT8:
+      case pprzlink::BaseType::INT16:
+      case pprzlink::BaseType::INT32:
+      case pprzlink::BaseType::UINT8:
+      case pprzlink::BaseType::UINT16:
+      case pprzlink::BaseType::UINT32:
+      case pprzlink::BaseType::FLOAT:
+      case pprzlink::BaseType::DOUBLE:
+        if (field.getType().isArray()) {
+          std::vector<double> vals;
+          for (auto val : values[i]) {
+            vals.push_back(boost::get<double>(val));
+          }
+          msg.addField(field.getName(), vals);
+        } else {
+          msg.addField(field.getName(), boost::get<double>(values[i][0]));
         }
+        break;
+      case pprzlink::BaseType::STRING:
+        msg.addField(field.getName(), boost::get<std::string>(values[i][0]));
+        break;
+      case pprzlink::BaseType::CHAR:
+        if (field.getType().isArray()) {
+          std::string vals;
+          size_t end = i + 1;
+          if (i + 1 == def.getNbFields()) {
+            end = values.size();
+          }
+
+          for (size_t j = i; j < end; j++) {
+            for (size_t k = 0; k < values[j].size(); k++) {
+              auto val = boost::get<std::string>(values[j][k]);
+              vals += val;
+              vals += ",";
+            }
+            vals.resize(vals.length() - 1);
+            vals += " ";
+          }
+          vals.resize(vals.length() - 1);
+
+          if (vals[0] == '"') {
+            vals = vals.substr(1, vals.size() - 2);
+          }
+          msg.addField(field.getName(), vals);
+
+        } else {
+          msg.addField(field.getName(), boost::get<std::string>(values[i][0]));
+        }
+        break;
+      default:
+        break;
     }
-
-
-    // Even faster!
-    auto add_msg = [&](auto& ctx) {
-        std::cout << boost::fusion::at_c<0>(_attr(ctx)) << " " << boost::fusion::at_c<2>(_attr(ctx)) << std::endl;
-    };
-    boost::iostreams::mapped_file_source file(argv[1]);
-    parse(file.begin(), file.end(), *((float_ >> ' ' >> int_ >> ' ' >> *~char_(" ") >> ' ' >> *~char_("\r\n") >> eol)[add_msg]));
-
-
-    std::cout << " - m_numLines = " << m_numLines << "\n";
-//    std::cout << " - mmap.size() = " << mmap.size() << "\n";
+  }
+  return msg;
 }
 
+
+
+int main(int argc, char *argv[])
+{
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <filename>\n";
+    return 1;
+  }
+
+  // Check if the data file exists
+  if (boost::filesystem::is_empty(argv[1])) {
+    std::cerr << " - Empty DATA file\n";
+    return 1;
+  }
+  std::cout << "LOG: " << argv[1] << "\n";
+
+  // Replace and check if the log file exists
+  std::string log_file = argv[1];
+  log_file.replace(log_file.end() - 4, log_file.end(), "log");
+  if (boost::filesystem::is_empty(log_file)) {
+    std::cerr << " - Empty LOG file\n";
+    return 1;
+  }
+
+  // Open the .log file in the xml parser
+  tinyxml2::XMLDocument xml;
+  xml.LoadFile(log_file.c_str());
+
+  // Check if the root element is configuration
+  tinyxml2::XMLElement *root = xml.RootElement();
+  std::string rootElem(root->Value());
+  if (rootElem != "configuration") {
+    std::cerr << "Root element is not configuration in xml messages file (found " + rootElem + ").";
+    return 1;
+  }
+
+  // Load message definitions from *.LOG / messages.xml
+  pprzlink::MessageDictionary *dict = new pprzlink::MessageDictionary(root->FirstChildElement("protocol"));
+
+  // Load all aircraft names
+  parse_airframe_list(root);
+
+
+
+  // GPS_INT message
+  auto gps_int = [&](auto & ctx) {
+    // auto timestamp = boost::fusion::at_c<0>(_attr(ctx));
+    auto ac_id = uint8_t(boost::fusion::at_c<1>(_attr(ctx)));
+    auto values = boost::fusion::at_c<2>(_attr(ctx));
+    auto msg = get_msg("GPS_INT", dict, values);
+    msg.setSenderId(ac_id);
+
+    // std::cout << " - GPS_INT: " << timestamp << " " << msg.toString() << "\n";
+  };
+
+  // INFO_MSG message
+  auto info_msg = [&](auto & ctx) {
+    auto timestamp = boost::fusion::at_c<0>(_attr(ctx));
+    auto ac_id = uint8_t(boost::fusion::at_c<1>(_attr(ctx)));
+    auto values = boost::fusion::at_c<2>(_attr(ctx));
+    auto msg = get_msg("INFO_MSG", dict, values);
+    msg.setSenderId(ac_id);
+
+    std::cout << " - INFO_MSG: " << timestamp << " " << msg.toString() << "\n";
+  };
+
+  // ROTORCRAFT_FP message
+  auto rotorcraft_fp = [&](auto & ctx) {
+    // auto timestamp = boost::fusion::at_c<0>(_attr(ctx));
+    auto ac_id = uint8_t(boost::fusion::at_c<1>(_attr(ctx)));
+    auto values = boost::fusion::at_c<2>(_attr(ctx));
+    auto msg = get_msg("ROTORCRAFT_FP", dict, values);
+    msg.setSenderId(ac_id);
+
+    // std::cout << " - ROTORCRAFT_FP: " << timestamp << " " << msg.toString() << "\n";
+  };
+
+  // STAB_ATTITUDE message
+  auto stab_attitude = [&](auto & ctx) {
+    auto timestamp = boost::fusion::at_c<0>(_attr(ctx));
+    auto ac_id = uint8_t(boost::fusion::at_c<1>(_attr(ctx)));
+    auto values = boost::fusion::at_c<2>(_attr(ctx));
+    auto msg = get_msg("STAB_ATTITUDE", dict, values);
+    msg.setSenderId(ac_id);
+
+    // std::cout << " - STAB_ATTITUDE: " << timestamp << " " << msg.toString() << "\n";
+  };
+
+  // AUTOPILOT_VERSION message
+  auto autopilot_version = [&](auto & ctx) {
+    auto timestamp = boost::fusion::at_c<0>(_attr(ctx));
+    auto ac_id = uint8_t(boost::fusion::at_c<1>(_attr(ctx)));
+    auto values = boost::fusion::at_c<2>(_attr(ctx));
+    auto msg = get_msg("AUTOPILOT_VERSION", dict, values);
+    msg.setSenderId(ac_id);
+
+    std::cout << " - AUTOPILOT_VERSION: " << timestamp << " " << msg.toString() << "\n";
+  };
+
+  // Add the parser
+  auto var_types = (double_ | lexeme[+~char_("\r\n")]);
+  auto var_options = (var_types % ',');
+  auto matcher = (float_ >> ' ' >> int_ >> ' ' >> "GPS_INT" >> ' ' >> (var_options % ' ') >> eol)[gps_int] \
+                 | (float_ >> ' ' >> int_ >> ' ' >> "INFO_MSG" >> ' ' >> (var_options % ' ') >> eol)[info_msg] \
+                  | (float_ >> ' ' >> int_ >> ' ' >> "ROTORCRAFT_FP" >> ' ' >> (var_options % ' ') >> eol)[rotorcraft_fp] \
+                  | (float_ >> ' ' >> int_ >> ' ' >> "STAB_ATTITUDE" >> ' ' >> (var_options % ' ') >> eol)[stab_attitude] \
+                  | (float_ >> ' ' >> int_ >> ' ' >> "AUTOPILOT_VERSION" >> ' ' >> (var_options % ' ') >> eol)[autopilot_version];
+  auto res = matcher | ( * ~char_("\r\n") >> eol);
+
+  // Parse the file
+  boost::iostreams::mapped_file_source file(argv[1]);
+  parse(file.begin(), file.end(), *(res));
+}
